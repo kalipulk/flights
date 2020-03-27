e251b76db83943c13647eb23ba7507d3c398b102
cmatchneer <cmatchneer@yahoo.com>
2020-03-27 13:51:01 -0400

    react issue

--- client/react_flights/src/App.js
+++ client/react_flights/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Nav from "./components/Nav";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/login";
+import SignUp from "./pages/signup";
+import Search from "./pages/search";
+import Profile from "./pages/profile";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path ="/search"component={Search} />
+          <Route exact path ="/profile"component={Profile} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
